72bbb61ec471f7edfea23dbc87b3e907431d27fb
Adriano-FER <adrianofp-94@hotmail.es>
2021-10-17 21:27:52 -0300

    fixing console errors

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => {
     return (
         <div>
             <ul className="topnav">
-             <li className="item"><a className="active" href="#home">Home</a></li>
-            <li className="item"><a href="#products">Products</a></li>
-             <li className="item"><a href="#cart">Cart</a></li>
-            <li className="item"><a href="#about">About</a></li>
-              <li className="item"><a href="#contact">Contact</a></li>
-             <li className="brand"><a>GamerForce <img src={logo} alt="Logo"></img></a> </li>
+             <li className="item"><a className="active" href="/#home">Home</a></li>
+            <li className="item"><a href="/#products">Products</a></li>
+             <li className="item"><a href="/#cart">Cart</a></li>
+            <li className="item"><a href="/#about">About</a></li>
+              <li className="item"><a href="/#contact">Contact</a></li>
+             <li className="brand">GamerForce <img src={logo} alt="Logo"></img> </li>
             <li > <CartWidget /></li>
         </ul>
         </div>
